de398cc4ac2e1b97ac1992fc0e98d2ef410fdf07
Charles Nicholson <charles.nicholson@gmail.com>
2025-10-19 22:23:42 -0400

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "cli.h"
+#include "tui.h"
 
 #include "tbb/flow_graph.h"
 #include "tbb/task_arena.h"
@@ -7,15 +8,22 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
+  envy::tui::init();
+
   try {
     auto cmd{ envy::cli_parse(argc, argv) };
     if (!cmd) { return EXIT_FAILURE; }
+
+    struct tui_scope {
+      tui_scope() { envy::tui::run(std::nullopt); }
+      ~tui_scope() { envy::tui::shutdown(); }
+    } scope;
 
     tbb::task_arena().execute([&cmd]() {
       tbb::flow::graph graph;
       cmd->schedule(graph);
       graph.wait_for_all();
-      cmd.reset(); // graph must outlive nodes owned by command
+      cmd.reset();  // graph must outlive nodes owned by command
     });
 
     return EXIT_SUCCESS;
