93c40aaadf5cc00fc12f47b9793ecc0762f88a2e
Charles Nicholson <charles.nicholson@gmail.com>
2025-11-29 23:18:10 -0500

    remove intel-only microoptimization, selectively remove noexecstack

--- cmake/deps/Libgit2.cmake
+++ cmake/deps/Libgit2.cmake
@@ -103,15 +103,12 @@ set(_envy_libgit2_warning_silencers
     $<$<COMPILE_LANG_AND_ID:C,AppleClang>:-Wno-single-bit-bitfield-constant-conversion>
     $<$<COMPILE_LANG_AND_ID:C,AppleClang>:-Wno-array-parameter>
     $<$<COMPILE_LANG_AND_ID:C,MSVC>:/wd5287>
+    # TSAN warning: atomic_thread_fence not supported with -fsanitize=thread
+    # Only active when TSAN is enabled; harmless otherwise
+    $<$<COMPILE_LANG_AND_ID:C,AppleClang>:-Wno-tsan>
+    $<$<COMPILE_LANG_AND_ID:C,Clang>:-Wno-tsan>
+    $<$<COMPILE_LANG_AND_ID:C,GNU>:-Wno-tsan>
 )
-# Silence TSAN warning about atomic_thread_fence not being supported with TSAN
-if(DEFINED SANITIZERS AND "${SANITIZERS}" MATCHES "thread")
-    list(APPEND _envy_libgit2_warning_silencers
-        $<$<COMPILE_LANG_AND_ID:C,AppleClang>:-Wno-tsan>
-        $<$<COMPILE_LANG_AND_ID:C,Clang>:-Wno-tsan>
-        $<$<COMPILE_LANG_AND_ID:C,GNU>:-Wno-tsan>
-    )
-endif()
 foreach(_libgit2_target IN ITEMS libgit2 libgit2package util ntlmclient http-parser xdiff)
     if(TARGET ${_libgit2_target})
         target_compile_options(${_libgit2_target} PRIVATE ${_envy_libgit2_warning_silencers})
