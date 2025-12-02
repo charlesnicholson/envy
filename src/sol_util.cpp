de306e598133399b93c53183a68ca6dcdf6831a8
Charles Nicholson <charles.nicholson@gmail.com>
2025-12-02 09:40:22 -0500

    better errors

--- src/sol_util.cpp
+++ src/sol_util.cpp
@@ -14,6 +14,28 @@ sol_state_ptr sol_util_make_lua_state() {
                       sol::lib::debug,
                       sol::lib::bit32,
                       sol::lib::io);
+
+  // Override error() and assert() to automatically include stack traces
+  lua->script(R"lua(
+do
+  local orig_error = error
+  local orig_assert = assert
+
+  _G.error = function(message, level)
+    level = (level or 1) + 1
+    return orig_error(debug.traceback(tostring(message), level), 0)
+  end
+
+  _G.assert = function(condition, message, ...)
+    if not condition then
+      message = message or "assertion failed"
+      return orig_assert(false, debug.traceback(tostring(message), 2))
+    end
+    return condition, message, ...
+  end
+end
+)lua");
+
   return lua;
 }
 
