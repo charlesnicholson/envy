1748194acd532e849f2144f564bf449478dd0aec
Charles Nicholson <charles.nicholson@gmail.com>
2025-12-07 19:13:39 -0500

    linux

--- examples/envy.lua
+++ examples/envy.lua
@@ -10,18 +10,20 @@ function append_lists(target, ...)
 end
 
 packages = {
-  { recipe = "local.armgcc@r0", source = "local.armgcc@r0.lua" },
-
-  {
-    recipe = "local.python@r0",
-    source = "local.python@r0.lua",
-    options = { version = "3.13.9", provide_python3 = true }
+  { recipe = "local.armgcc@r0", source = "local.armgcc@r0.lua",
+    options = { version = "14.3.rel1" }
   },
 
   {
     recipe = "local.python@r0",
     source = "local.python@r0.lua",
-    options = { version = "3.14.0" }
+    options = { version = "3.13.11", provide_python3 = true }
+  },
+
+  {
+    recipe = "local.python@r0",
+    source = "local.python@r0.lua",
+    options = { version = "3.14.2" }
   },
 
   { recipe = "local.ninja@r0",  source = "local.ninja@r0.lua" },
@@ -38,4 +40,16 @@ if ENVY_PLATFORM == "darwin" then
     },
   }
   append_lists(packages, mac_packages)
+end
+
+if ENVY_PLATFORM == "linux" then
+  apt_packages = {
+    {
+      recipe = "local.apt@r0",
+      source = "local.apt@r0.lua",
+      options = { packages = {
+        "libglib2.0-0", "libglib2.0-dev", "libudev-dev", "libusb-1.0-0.dev" } }
+    },
+  }
+  append_lists(packages, apt_packages)
 end
--- examples/local.armgcc@r0.lua
+++ examples/local.armgcc@r0.lua
@@ -1,9 +1,25 @@
 identity = "local.armgcc@r0"
 
-fetch = {
-  source =
-  "https://developer.arm.com/-/media/Files/downloads/gnu/14.3.rel1/binrel/arm-gnu-toolchain-14.3.rel1-darwin-arm64-arm-none-eabi.tar.xz",
-  sha256 = "30f4d08b219190a37cded6aa796f4549504902c53cfc3c7e044a8490b6eba1f7"
+local sha256_fingerprints = {
+  ["14.3.rel1-darwin-arm64"] =
+    "30f4d08b219190a37cded6aa796f4549504902c53cfc3c7e044a8490b6eba1f7",
+  ["14.3.rel1-x86_64"] = "8f6903f8ceb084d9227b9ef991490413014d991874a1e34074443c2a72b14dbd",
 }
+
+local url_prefix = "https://developer.arm.com/-/media/Files/downloads/gnu/"
+local arch = "-" .. ({darwin="darwin-arm64", linux=ENVY_ARCH})[ENVY_PLATFORM]
+local tail = "-arm-none-eabi." .. ((ENVY_PLATFORM == "windows") and "zip" or "tar.xz")
+
+fetch = function(ctx, opts)
+  local filename = "arm-gnu-toolchain-" .. opts.version .. arch .. tail
+
+  local fingerprint = sha256_fingerprints[opts.version .. arch]
+  assert(fingerprint)
+
+  return {
+    source = url_prefix .. opts.version .. "/binrel/" .. filename,
+    sha256 = fingerprint
+  }
+end
 
 stage = { strip = 1 }
