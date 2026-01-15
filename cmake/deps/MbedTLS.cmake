520f1d1dae7692b79e0ab312e05ed495cd757f6c
Charles Nicholson <charles.nicholson@gmail.com>
2026-01-15 00:00:52 -0500

    force mbedtls version

--- cmake/deps/MbedTLS.cmake
+++ cmake/deps/MbedTLS.cmake
@@ -60,6 +60,7 @@ if(DEFINED mbedtls_SOURCE_DIR AND DEFINED mbedtls_BINARY_DIR)
         "${MBEDCRYPTO_LIBRARY}"
         CACHE STRING "" FORCE)
     set(MBEDTLS_FOUND TRUE CACHE BOOL "" FORCE)
+    set(MBEDTLS_VERSION "${ENVY_MBEDTLS_VERSION}" CACHE STRING "" FORCE)
     unset(_mbedcrypto_lib)
     unset(_mbedtls_libdir)
     unset(_mbedtls_lib)
